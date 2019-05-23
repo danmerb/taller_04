8e33cc29fbb5e2a29bc28cfb7b28ca8d94c7c7ec
danmerb <00030717@uca.edu.sv>
2019-05-22 23:11:49 -0600

    comentando

--- app/src/main/java/com/example/android/Biblioteca/WordViewModel.kt
+++ app/src/main/java/com/example/android/Biblioteca/WordViewModel.kt
@@ -32,12 +32,9 @@ import kotlinx.coroutines.launch
 
 class WordViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: BookRepository
-    // Using LiveData and caching what getAlphabetizedWords returns has several benefits:
-    // - We can put an observer on the data (instead of polling for changes) and only update the
-    //   the UI when the data actually changes.
-    // - Repository is completely separated from the UI through the ViewModel.
-    val allWords: LiveData<List<Word>>
+    //private val repository: BookRepository
+
+    //val allWords: LiveData<List<Word>>
 
     init {
         //val wordsDao = BookRoomDatabase.getDatabase(application, viewModelScope).wordDao()
@@ -49,6 +46,6 @@ class WordViewModel(application: Application) : AndroidViewModel(application) {
      * Launching a new coroutine to insert the data in a non-blocking way
      */
     fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
-        repository.insert(word)
+       // repository.insert(word)
     }
 }
