7025f088a49f876fc9bf7ffc53bd3086a89a8e6d
RFuentes22 <00062217@uca.edu.sv>
2019-05-25 23:13:52 -0600

    Working RV

--- app/src/main/java/com/example/android/Biblioteca/MainActivity.kt
+++ app/src/main/java/com/example/android/Biblioteca/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android.Biblioteca
 
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -9,20 +10,18 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.Biblioteca.Adapter.BookAdapter
-import com.example.android.Biblioteca.Room.Entity.Author
 import com.example.android.Biblioteca.Room.Entity.Book
-import com.example.android.Biblioteca.Room.Entity.Editorial
-import com.example.android.Biblioteca.Room.Entity.Tag
 import com.example.android.Biblioteca.ViewModel.BookViewModel
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewAdapter: BookAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
-    private lateinit var libros: MutableList<Book>
+    //private lateinit var libros: MutableList<Book>
 
     private val newBookActivityRequestCode = 1
     private lateinit var bookViewModel: BookViewModel
@@ -31,12 +30,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val recyclerView = findViewById<RecyclerView>(R.id.rv_book_list)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         val adapter = BookAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-
 
 
         val viewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
@@ -62,77 +59,60 @@ class MainActivity : AppCompatActivity() {
              }
          })*/
         viewModel.getAllBook().observe(this, Observer { book ->
+
+            book?.let { adapter.setBooks(it) }
             for (book in book) {
                 Log.d("Lista de book", book.toString())
+
+                var libros = if (book.isbn > 0) {
+
+                    MutableList(2) { i ->
+                        Book(book.isbn,
+                                book.titleEnglish,
+                                book.titleSpanish,
+                                book.caratula,
+                                book.edition,
+                                book.author,
+                                book.tag,
+                                book.editorial,
+                                book.estado,
+                                book.resumeEnglish,
+                                book.resumeSpanish
+
+
+                        )
+
+
+                    }
+                } else {
+                    MutableList(2) { i ->
+                        Book(0, "N/A", "N/A", "N/A", 0, 0, 0, 0, false, "N/A", "N/A")
+                    }
+                }
+                //initRecycler(libros)
             }
+
+
         })
 
 
+
+
+
     }
+/*
+    fun initRecycler(libros: MutableList<Book>) {
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = BookAdapter(Context)
 
+        rv_book_list.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+    }*/
 }
-
-
-//initRecycler()
-
-/**
-val toolbar = findViewById<Toolbar>(R.id.toolbar)
-setSupportActionBar(toolbar)
-
-val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-val adapter = WordListAdapter(this)
-recyclerView.adapter = adapter
-recyclerView.layoutManager = LinearLayoutManager(this)
-
-// Get a new or existing ViewModel from the ViewModelProvider.
-wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
-
-// Add an observer on the LiveData returned by getAlphabetizedWords.
-// The onChanged() method fires when the observed data changes and the activity is
-// in the foreground.
-wordViewModel.allWords.observe(this, Observer { words ->
-// Update the cached copy of the words in the adapter.
-words?.let { adapter.setWords(it) }
-})
-
-val fab = findViewById<FloatingActionButton>(R.id.fab)
-fab.setOnClickListener {
-val intent = Intent(this@MainActivity, NewBookActivity::class.java)
-startActivityForResult(intent, newWordActivityRequestCode)
-}
-}
-
-override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
-super.onActivityResult(requestCode, resultCode, intentData)
-
-if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
-intentData?.let { data ->
-val word = Word(data.getStringExtra(NewBookActivity.EXTRA_REPLY))
-wordViewModel.insert(word)
-}
-} else {
-Toast.makeText(
-applicationContext,
-R.string.empty_not_saved,
-Toast.LENGTH_LONG
-).show()
-
- */
-
-
-/*fun initRecycler() {
-    viewManager = LinearLayoutManager(this)
-    viewAdapter = BookAdapter(libros)
-
-    rv_book_list.apply {
-        setHasFixedSize(true)
-        layoutManager = viewManager
-        adapter = viewAdapter
-    }
-
-}*/
-
-
 
 
 
